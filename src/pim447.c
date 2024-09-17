a5adb00322be87fed75dccde82a67cda2c0b169f
t0bybr <4039962+t0bybr@users.noreply.github.com>
2024-09-17 21:24:57 +0200

    Refactor pim447.c: Update trackball interrupt GPIO property name

--- src/pim447.c
+++ src/pim447.c
@@ -1,123 +1,104 @@
 #define DT_DRV_COMPAT pimoroni_pim447
 
 #include <zephyr/device.h>
-#include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/i2c.h>
-#include <zephyr/input/input.h>
-
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(pimoroni_pim447, CONFIG_SENSOR_LOG_LEVEL);
+LOG_MODULE_REGISTER(pim447, CONFIG_SENSOR_LOG_LEVEL);
 
-#define PIMORONI_PIM447_I2C_ADDRESS 0x0A
-
-// Register addresses
-#define REG_LEFT 0x00
-#define REG_RIGHT 0x01
-#define REG_UP 0x02
-#define REG_DOWN 0x03
-#define REG_SWITCH 0x04
-
-struct pimoroni_pim447_data {
-    const struct device *i2c_dev;
-    int16_t dx;
-    int16_t dy;
-    bool pressed;
-};
-
-struct pimoroni_pim447_config {
+struct pim447_config {
     struct i2c_dt_spec i2c;
+    struct gpio_dt_spec int_gpio;
 };
 
-static int pimoroni_pim447_sample_fetch(const struct device *dev, enum sensor_channel chan)
-{
-    struct pimoroni_pim447_data *data = dev->data;
-    const struct pimoroni_pim447_config *cfg = dev->config;
-    uint8_t buf[5];
+struct pim447_data {
+    const struct device *dev;
+    struct k_work work;
+    struct gpio_callback int_gpio_cb;
+};
+
+/* Work handler function that logs when the trackball is moved */
+static void pim447_work_handler(struct k_work *work) {
+    struct pim447_data *data = CONTAINER_OF(work, struct pim447_data, work);
+
+    /* Log that the trackball movement was detected */
+    LOG_INF("Trackball moved");
+}
+
+/* GPIO callback function triggered by the interrupt */
+static void pim447_gpio_callback(const struct device *port, struct gpio_callback *cb,
+                                 gpio_port_pins_t pins) {
+    struct pim447_data *data = CONTAINER_OF(cb, struct pim447_data, int_gpio_cb);
+
+    /* Schedule the work handler to process the movement */
+    k_work_submit(&data->work);
+}
+
+/* Initialization function for the PIM447 driver */
+static int pim447_init(const struct device *dev) {
+    const struct pim447_config *config = dev->config;
+    struct pim447_data *data = dev->data;
     int ret;
 
-    ret = i2c_burst_read_dt(&cfg->i2c, REG_LEFT, buf, 5);
-    if (ret < 0) {
-        LOG_ERR("Failed to read movement data from PIM447");
-        return ret;
-    }
+    data->dev = dev;
 
-    // Calculate dx and dy
-    int16_t dx = (int16_t)buf[1] - (int16_t)buf[0];
-    int16_t dy = (int16_t)buf[3] - (int16_t)buf[2];
-
-    // Update button state
-    bool pressed = (buf[4] & 0x80) != 0;
-
-    // Log movement if there's any change
-    if (dx != 0 || dy != 0 || pressed != data->pressed) {
-        LOG_INF("Trackball movement: dx=%d, dy=%d, pressed=%d", dx, dy, pressed);
-    }
-
-    // Update data
-    data->dx = dx;
-    data->dy = dy;
-    data->pressed = pressed;
-
-    return 0;
-}
-
-
-static int pimoroni_pim447_channel_get(const struct device *dev, enum sensor_channel chan, struct sensor_value *val)
-{
-    struct pimoroni_pim447_data *data = dev->data;
-
-    switch (chan) {
-    case SENSOR_CHAN_POS_DX:
-        val->val1 = data->dx;
-        val->val2 = 0;
-        break;
-    case SENSOR_CHAN_POS_DY:
-        val->val1 = data->dy;
-        val->val2 = 0;
-        break;
-    case SENSOR_CHAN_PROX:
-        val->val1 = data->pressed ? 1 : 0;
-        val->val2 = 0;
-        break;
-    default:
-        return -ENOTSUP;
-    }
-
-    return 0;
-}
-
-static const struct sensor_driver_api pimoroni_pim447_api = {
-    .sample_fetch = pimoroni_pim447_sample_fetch,
-    .channel_get = pimoroni_pim447_channel_get,
-};
-
-static int pimoroni_pim447_init(const struct device *dev)
-{
-    const struct pimoroni_pim447_config *cfg = dev->config;
-
-    LOG_INF("pim447 am Start");
-
-    if (!device_is_ready(cfg->i2c.bus)) {
-        LOG_ERR("I2C bus not ready");
+    /* Check if the I2C device is ready */
+    if (!device_is_ready(config->i2c.bus)) {
+        LOG_ERR("I2C bus device is not ready");
         return -ENODEV;
     }
 
+    /* Check if the interrupt GPIO device is ready */
+    if (!device_is_ready(config->int_gpio.port)) {
+        LOG_ERR("Interrupt GPIO device is not ready");
+        return -ENODEV;
+    }
+
+    /* Configure the interrupt GPIO pin */
+    ret = gpio_pin_configure_dt(&config->int_gpio, GPIO_INPUT);
+    if (ret) {
+        LOG_ERR("Failed to configure interrupt GPIO");
+        return ret;
+    }
+
+    /* Initialize the GPIO callback */
+    gpio_init_callback(&data->int_gpio_cb, pim447_gpio_callback,
+                       BIT(config->int_gpio.pin));
+
+    /* Add the GPIO callback */
+    ret = gpio_add_callback(config->int_gpio.port, &data->int_gpio_cb);
+    if (ret) {
+        LOG_ERR("Failed to add GPIO callback");
+        return ret;
+    }
+
+    /* Configure the GPIO interrupt */
+    ret = gpio_pin_interrupt_configure_dt(&config->int_gpio, GPIO_INT_EDGE_TO_ACTIVE);
+    if (ret) {
+        LOG_ERR("Failed to configure GPIO interrupt");
+        return ret;
+    }
+
+    /* Initialize the work handler */
+    k_work_init(&data->work, pim447_work_handler);
+
+    /* Log that the driver has been initialized */
+    LOG_INF("PIM447 driver initialized");
+
     return 0;
 }
 
-#define PIMORONI_PIM447_INIT(inst)                                            \
-    static struct pimoroni_pim447_data pimoroni_pim447_data_##inst;           \
-    static const struct pimoroni_pim447_config pimoroni_pim447_config_##inst = { \
-        .i2c = I2C_DT_SPEC_INST_GET(inst),                                    \
-    };                                                                        \
-    DEVICE_DT_INST_DEFINE(inst,                                               \
-                          pimoroni_pim447_init,                               \
-                          NULL,                                               \
-                          &pimoroni_pim447_data_##inst,                       \
-                          &pimoroni_pim447_config_##inst,                     \
-                          POST_KERNEL,                                        \
-                          CONFIG_SENSOR_INIT_PRIORITY,                        \
-                          &pimoroni_pim447_api);
+/* Device configuration */
+static const struct pim447_config pim447_config = {
+    .i2c = I2C_DT_SPEC_INST_GET(0),
+    .int_gpio = GPIO_DT_SPEC_INST_GET(0, int_gpios),
+};
 
-DT_INST_FOREACH_STATUS_OKAY(PIMORONI_PIM447_INIT)
+/* Device data */
+static struct pim447_data pim447_data;
+
+/* Device initialization macro */
+DEVICE_DT_INST_DEFINE(0, pim447_init, NULL, &pim447_data, &pim447_config,
+                      POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEVICE, NULL);
