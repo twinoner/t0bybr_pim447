80fcaef59d020f24373b832483ff039387888a17
t0bybr <4039962+t0bybr@users.noreply.github.com>
2024-09-18 21:18:41 +0200

    Refactor pim447.h: Remove unused register addresses and bit masks

--- src/pim447.c
+++ src/pim447.c
@@ -6,6 +6,39 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+
+/* Register Addresses */
+#define REG_LED_RED     0x00
+#define REG_LED_GRN     0x01
+#define REG_LED_BLU     0x02
+#define REG_LED_WHT     0x03
+#define REG_LEFT        0x04
+#define REG_RIGHT       0x05
+#define REG_UP          0x06
+#define REG_DOWN        0x07
+#define REG_SWITCH      0x08
+#define REG_USER_FLASH  0xD0
+#define REG_FLASH_PAGE  0xF0
+#define REG_INT         0xF9
+#define REG_CHIP_ID_L   0xFA
+#define REG_CHIP_ID_H   0xFB
+#define REG_VERSION     0xFC
+#define REG_I2C_ADDR    0xFD
+#define REG_CTRL        0xFE
+
+/* Bit Masks */
+#define MSK_SWITCH_STATE    0b10000000
+
+/* Interrupt Masks */
+#define MSK_INT_TRIGGERED   0b00000001
+#define MSK_INT_OUT_EN      0b00000010
+
+/* Control Masks */
+#define MSK_CTRL_SLEEP      0b00000001
+#define MSK_CTRL_RESET      0b00000010
+#define MSK_CTRL_FREAD      0b00000100
+#define MSK_CTRL_FWRITE     0b00001000
+
 LOG_MODULE_REGISTER(pim447, CONFIG_SENSOR_LOG_LEVEL);
 
 struct pim447_config {
