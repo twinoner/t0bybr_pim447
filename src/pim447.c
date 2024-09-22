0deb2a112b1f7bfef20cdb3165564de1df25ea42
t0bybr <4039962+t0bybr@users.noreply.github.com>
2024-09-22 08:54:37 +0200

    changed to hid.h

--- src/pim447.c
+++ src/pim447.c
@@ -9,6 +9,8 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/input/input.h>
 
+#include <zmk/hid.h>
+
 
 /* Register Addresses */
 #define REG_LED_RED     0x00
@@ -112,8 +114,9 @@ static void pim447_work_handler(struct k_work *work) {
         data->sw_pressed_prev = sw_pressed;
     }
 
-    input_report_rel(dev, REL_X, delta_x, false, K_NO_WAIT);
-    input_report_rel(dev, REL_Y, delta_y, false, K_NO_WAIT);
+    struct zmk_hid_mouse_report *report = zmk_hid_get_mouse_report();
+    report->body.x = delta_x;
+    report->body.y = delta_y;
 
     /* Read and clear the INT status register if necessary */
     uint8_t int_status;
