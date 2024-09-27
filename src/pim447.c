665d82365e7867a3e119aea0acfabf10676eb58f
t0bybr <4039962+t0bybr@users.noreply.github.com>
2024-09-27 10:49:11 +0200

    Refactor pim447.c to remove unnecessary logging of MOVE_FACTOR and improve speed conversion

--- src/pim447.c
+++ src/pim447.c
@@ -48,7 +48,6 @@ K_MUTEX_DEFINE(variable_mutex);
 
 LOG_MODULE_REGISTER(pim447, LOG_LEVEL_DBG);
 
-#define MOVE_FACTOR    DT_PROP(DT_INST(0, pimoroni_pim447), move_factor);
 
 
 
@@ -73,7 +72,6 @@ static int pim447_enable_interrupt(const struct pim447_config *config, bool enab
 
 static int32_t convert_speed(int32_t value)
 {
-    LOG_INF("MOVE_FACTOR: %d", MOVE_FACTOR);
     LOG_INF("Converting speed: %d", value);
 
     bool negative = (value < 0);
@@ -125,6 +123,8 @@ static void pim447_work_handler(struct k_work *work) {
     LOG_INF("Raw data: LEFT=%d, RIGHT=%d, UP=%d, DOWN=%d, SWITCH=0x%02X",
             buf[0], buf[1], buf[2], buf[3], buf[4]);
 
+
+    int32_t MOVE_FACTOR = 1;
 
     // Calculate movement deltas
     int32_t delta_x_raw = (int32_t)buf[1] - (int32_t)buf[0]; // Right - Left
