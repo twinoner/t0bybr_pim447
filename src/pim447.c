72adce5c2bb6d7cf6940efc110e97a567f9c8545
t0bybr <4039962+t0bybr@users.noreply.github.com>
2024-09-28 21:29:58 +0200

    Refactor Pimoroni PIM447 trackball support: Update device tree bindings and driver
    This commit updates the device tree bindings and driver for the Pimoroni PIM447 trackball. The changes include:
    - Deleting the old device tree binding file for zmk,pimoroni-pim447
    - Modifying the driver file to use the correct device tree compatible property (pimoroni_pim447)
    - Adding a new device tree binding file for pimoroni,pim447
    
    These changes ensure that the trackball is properly supported in the system.

--- src/pim447.c
+++ src/pim447.c
@@ -1,6 +1,6 @@
 /* pimoroni_pim447.c - Driver for Pimoroni PIM447 Trackball */
 
-#define DT_DRV_COMPAT zmk_pimoroni_pim447
+#define DT_DRV_COMPAT pimoroni_pim447
 
 #include <zephyr/device.h>
 #include <zephyr/input/input.h>
